496643fac853955c36b8f5a2081bbea1e74c28fa
AMRITESH KUMAR <157412986+AmriteshKr24@users.noreply.github.com>
2024-03-05 01:40:25 +0530

    Update main.py

--- chatbot/main.py
+++ chatbot/main.py
@@ -35,7 +35,7 @@ def index():
 
         # Generate chatbot responseWe
         bot_response = chatbot_response(f'Act as an chatbot of Wellness Buddy website used for telling information about '
-                                        f'cancer, tuberculosis and diabetes and their different types, symptoms, causes and their treatments and also say what are the possible diseases or predict from given symptoms  do not reply to inrelevant queries, query = "{user_query}"')
+                                        f'cancer, tuberculosis and diabetes and their different types, symptoms, causes and their treatments and with that also say what are the possible diseases or predict from given symptoms for all diseases and also say info about different diseases symptoms do not reply to inrelevant queries, query = "{user_query}"')
 
         # Format bot response
         bot_response = to_markdown(bot_response)
